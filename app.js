a257cf7029d40cb73d3af6b7e5ac3fd25dbc1dd1
Kasia Nadolna <katarzyna.nadolna111@gmail.com>
2025-08-26 06:56:56 +0200

    slider and about section created

---
+++ app.js
@@ -0,0 +1,13 @@
+// const img = document.querySelector('.img-about');
+
+// const observer = new IntersectionObserver((entries, observer) => {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add('active');
+//       observer.unobserve(entry.target);
+//     }
+//   });
+// }, { threshold: 0.1 });
+
+// console.log(img);
+// observer.observe(img);
